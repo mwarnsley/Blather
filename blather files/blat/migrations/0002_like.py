0c536c5d5a34e46949569088ff491694d90b8d4a
Marcus Warnsley <mwarnsley@Marcuss-MacBook-Pro.local>
2016-08-29 18:15:22 -0400

    Editing the migration files along with the database, more authentication etc

--- blather files/blat/migrations/0002_like.py
+++ blather files/blat/migrations/0002_like.py
@@ -7,16 +7,15 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('blat', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Blat',
+            name='Like',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('text', models.TextField()),
-                ('created_on', models.DateTimeField(auto_now_add=True)),
-                ('via', models.URLField(blank=True)),
+                ('blat', models.ForeignKey(to='blat.Blat')),
             ],
             options={
             },
--- blather files/blat/models.py
+++ blather files/blat/models.py
@@ -3,16 +3,21 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Blat(models.Model):
-    text = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    via = models.URLField(blank=True)
-    created_by = models.ForeignKey(User)
+	text = models.TextField()
+	created_on = models.DateTimeField(auto_now_add=True)
+	via = models.URLField(blank=True)
+	created_by = models.ForeignKey(User)
 
-    def total_likes(self):
-    	return self.like_set.count()
+	def __unicode__(self):
+		return self.text[:50]
 
-    def __unicode__(self):
-    	return self.text[:50]
+	def total_likes(self):
+		return self.like_set.count()
 
 class Like(models.Model):
 	blat = models.ForeignKey(Blat)
+
+class Profile(models.Model):
+	user = models.OneToOneField(User)
+	bio = models.TextField(blank=True)
+	blog = models.URLField(blank=True)
