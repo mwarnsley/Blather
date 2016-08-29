20f721c989586bc2fbd66cace8074c188e649468
Marcus Warnsley <mwarnsley@Marcuss-MacBook-Pro.local>
2016-08-29 12:01:46 -0400

    Creating the classes to use for database integration

--- blather files/blat/models.py
+++ blather files/blat/models.py
@@ -1,5 +1,13 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+class Blat(models.Model):
+    text = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    via = models.URLField(blank=True)
+
+    def total_likes(self):
+    	return self.like_set.count()
+
+class Like(models.Model):
+	blat = models.ForeignKey(Blat)
